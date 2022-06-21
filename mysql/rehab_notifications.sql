445e65dbe5d821571b214d1b44c65e80c1f5ba44
quannb85005 <quannb85005@co-well.vn>
2022-06-21 16:24:12 +0700

    report manage

--- mysql/rehab_notifications.sql
+++ mysql/rehab_notifications.sql
@@ -28,6 +28,7 @@ CREATE TABLE `notifications` (
   `notifiable_type` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `notifiable_id` bigint(20) unsigned NOT NULL,
   `data` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `solved` varchar(45) COLLATE utf8mb4_unicode_ci DEFAULT '0',
   `read_at` timestamp NULL DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL,
@@ -42,7 +43,7 @@ CREATE TABLE `notifications` (
 
 LOCK TABLES `notifications` WRITE;
 /*!40000 ALTER TABLE `notifications` DISABLE KEYS */;
-INSERT INTO `notifications` VALUES ('0af6c42f-189b-4641-ab8c-1400a0f9151e','App\\Notifications\\ReportComment','App\\Models\\User',10,'{\"post_id\":29,\"comment_id\":25,\"content\":\"c\\u01b0\\u1eddi g\\u00ec\",\"user_id\":2,\"comment_user_name\":\"Nguy\\u1ec5n Thanh Li\\u00eam\",\"comment_user_avatar\":\"..\\/..\\/uploads\\/62af2722e317612_01_2019_11_08_54_075889.jpeg\",\"user_name\":\"Nguy\\u1ec5n B\\u00e1 Qu\\u00e2n\",\"avatar\":\"..\\/..\\/uploads\\/62a9c399bb202269691465_2136015413203455_4731070248963071192_n.jpg\"}',NULL,'2022-06-20 08:36:58','2022-06-20 08:36:58'),('bdb896f7-1fa2-42fb-a0bf-9ef7e4cea837','App\\Notifications\\ReportComment','App\\Models\\User',10,'{\"post_id\":29,\"comment_id\":25,\"content\":\"c\\u01b0\\u1eddi g\\u00ec\",\"user_id\":2,\"comment_user_name\":\"Nguy\\u1ec5n Thanh Li\\u00eam\",\"comment_user_avatar\":\"..\\/..\\/uploads\\/62af2722e317612_01_2019_11_08_54_075889.jpeg\",\"user_name\":\"Nguy\\u1ec5n B\\u00e1 Qu\\u00e2n\",\"avatar\":\"..\\/..\\/uploads\\/62a9c399bb202269691465_2136015413203455_4731070248963071192_n.jpg\"}',NULL,'2022-06-20 08:28:28','2022-06-20 08:28:28'),('e917990d-453d-4619-80fb-0d1a1f609600','App\\Notifications\\ReportPost','App\\Models\\User',10,'{\"post_id\":34,\"user_id\":2,\"title\":\"\\u00e1dasdasd\",\"type\":\"Chia s\\u1ebb\",\"user_name\":\"Nguy\\u1ec5n B\\u00e1 Qu\\u00e2n\",\"avatar\":\"..\\/..\\/uploads\\/62a9c399bb202269691465_2136015413203455_4731070248963071192_n.jpg\",\"post_user_name\":\"Nguy\\u1ec5n Thanh Li\\u00eam\"}',NULL,'2022-06-20 08:49:00','2022-06-20 08:49:00');
+INSERT INTO `notifications` VALUES ('0af6c42f-189b-4641-ab8c-1400a0f9151e','App\\Notifications\\ReportComment','App\\Models\\User',10,'{\"post_id\":29,\"comment_id\":25,\"content\":\"c\\u01b0\\u1eddi g\\u00ec\",\"user_id\":2,\"comment_user_name\":\"Nguy\\u1ec5n Thanh Li\\u00eam\",\"comment_user_avatar\":\"..\\/..\\/uploads\\/62af2722e317612_01_2019_11_08_54_075889.jpeg\",\"user_name\":\"Nguy\\u1ec5n B\\u00e1 Qu\\u00e2n\",\"avatar\":\"..\\/..\\/uploads\\/62a9c399bb202269691465_2136015413203455_4731070248963071192_n.jpg\"}','0',NULL,'2022-06-20 08:36:58','2022-06-20 08:36:58'),('bdb896f7-1fa2-42fb-a0bf-9ef7e4cea837','App\\Notifications\\ReportComment','App\\Models\\User',10,'{\"post_id\":29,\"comment_id\":25,\"content\":\"c\\u01b0\\u1eddi g\\u00ec\",\"user_id\":2,\"comment_user_name\":\"Nguy\\u1ec5n Thanh Li\\u00eam\",\"comment_user_avatar\":\"..\\/..\\/uploads\\/62af2722e317612_01_2019_11_08_54_075889.jpeg\",\"user_name\":\"Nguy\\u1ec5n B\\u00e1 Qu\\u00e2n\",\"avatar\":\"..\\/..\\/uploads\\/62a9c399bb202269691465_2136015413203455_4731070248963071192_n.jpg\"}','1',NULL,'2022-06-20 08:28:28','2022-06-21 07:53:21'),('e917990d-453d-4619-80fb-0d1a1f609600','App\\Notifications\\ReportPost','App\\Models\\User',10,'{\"post_id\":34,\"user_id\":2,\"title\":\"\\u00e1dasdasd\",\"type\":\"Chia s\\u1ebb\",\"user_name\":\"Nguy\\u1ec5n B\\u00e1 Qu\\u00e2n\",\"avatar\":\"..\\/..\\/uploads\\/62a9c399bb202269691465_2136015413203455_4731070248963071192_n.jpg\",\"post_user_name\":\"Nguy\\u1ec5n Thanh Li\\u00eam\"}','0',NULL,'2022-06-20 08:49:00','2022-06-20 08:49:00');
 /*!40000 ALTER TABLE `notifications` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -55,4 +56,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-06-20 15:59:44
+-- Dump completed on 2022-06-21 16:23:54
