5f1a02e1807e828c06b2b4fd95d6726c703ba39b
quannb99 <quannb85005@co-well.vn>
2022-07-03 11:35:42 +0700

    fix bug

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    protected $fillable = ['*'];
+    protected $guarded = ['id'];
     public function comments()
     {
         return $this->hasMany('App\Models\Comment');
