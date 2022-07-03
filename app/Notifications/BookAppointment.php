30a1bd595d9bafa5a71c50960e5faf0a5b2c55f0
quannb99 <quannb85005@co-well.vn>
2022-07-03 17:34:40 +0700

    appointment noti

---
+++ app/Notifications/BookAppointment.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Notifications;
+
+use App\Models\Appointment;
+use App\Models\Comment;
+use App\Models\Post;
+use App\Models\User;
+use Illuminate\Bus\Queueable;
+use Illuminate\Notifications\Notification;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+
+class BookAppointment extends Notification
+{
+    use Queueable;
+
+    protected $user;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct(User $user, Appointment $appointment)
+    {
+        $this->appointment = $appointment;
+        $this->user = $user;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        // return ['mail'];
+        return ['database', 'broadcast'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        return (new MailMessage)
+                    ->line('The introduction to the notification.')
+                    ->action('Notification Action', url('/'))
+                    ->line('Thank you for using our application!');
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+            'user_name' => $this->user->name,
+            'avatar' => $this->user->avatar,
+            'created_at' => $this->appointment->created_at,
+            'start_at' => $this->appointment->start_at,
+            'end_at' => $this->appointment->end_at,
+        ];
+    }
+}
